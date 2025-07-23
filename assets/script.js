bdb6c9b31a7e5c02f533a221a6c4d88264462012
RichardEscobe2 <104027505+RichardEscobe2@users.noreply.github.com>
2025-07-23 17:57:07 -0600

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// Funcionalidad JavaScript personalizada
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Documento cargado y listo.');
+    // Aquí puedes añadir más interacciones de JavaScript si son necesarias.
+});
