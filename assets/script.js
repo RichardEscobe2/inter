25bcd0d8bc2865b3681beb7f323b7b9592150d0d
RichardEscobe2 <104027505+RichardEscobe2@users.noreply.github.com>
2025-07-25 21:22:52 -0600

    Delete assets directory

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-// Funcionalidad JavaScript personalizada
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Documento cargado y listo.');
-    // Aquí puedes añadir más interacciones de JavaScript si son necesarias.
-});
