6ea64cb5301e65d4a29259a77e9ecf8dbb03ef16
RichardEscobe2 <104027505+RichardEscobe2@users.noreply.github.com>
2025-07-25 21:21:22 -0600

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="asstes/estilo.css">
     <!-- Font Awesome for social icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
