88887efcb2bb783e1f606e6fb81543f9d2d9328f
RichardEscobe2 <104027505+RichardEscobe2@users.noreply.github.com>
2025-08-01 11:27:35 -0600

    Update login.php

--- login.php
+++ login.php
@@ -8,102 +8,60 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="asstes/estilo.css">
-    <!-- Font Awesome for social icons -->
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
-<body>
-
+<body class="bg-login">
 
 <div id="navbar"></div>
 
-    <main class="container mt-4 auth-page">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        Iniciar Sesión
+<main class="auth-page d-flex align-items-center justify-content-center">
+
+    <div class="col-md-8 col-lg-6 col-xl-5">
+        <div class="card">
+            <div class="card-header text-center">
+                <img src="asstes/logo.png" style="height: 40px;" alt="Logo FindLab"><br>
+                Iniciar Sesión
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="form-group">
+                        <label for="email">Correo Electrónico</label>
+                        <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo">
                     </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group">
-                                <label for="email">Correo Electrónico</label>
-                                <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Contraseña</label>
-                                <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Aun no tienes cuenta?, <a href="signup.php">Registrate!</a></label>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">Iniciar Sesión</button>
-                        </form>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label>¿Aún no tienes cuenta? <a href="signup.php">¡Regístrate!</a></label>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Iniciar Sesión</button>
+                </form>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 
+<div id="foot"></div>
 
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="assets/script.js"></script>
 
+<script>
+    fetch('poo/navbar.php')
+        .then(res => res.text())
+        .then(data => document.getElementById('navbar').innerHTML = data);
+</script>
 
-
-
-
-
-     <div id = "foot"></div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- Bootstrap JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Custom JS -->
-    <script src="assets/script.js"></script>
-
-
-
-
-
-
-
-
-
-
-
-        <script>
-        fetch ('poo/navbar.php')
-        .then(response => response.text())
-        .then(data => {
-
-            document.getElementById('navbar').innerHTML = data;
-        });
-    </script>
-
-    <script>
-        fetch ('poo/footer.php')
-        .then(response => response.text())
-        .then(data => {
-
-            document.getElementById('foot').innerHTML = data;
-        });
-    </script>
+<script>
+    fetch('poo/footer.php')
+        .then(res => res.text())
+        .then(data => document.getElementById('foot').innerHTML = data);
+</script>
 
 </body>
 </html>
